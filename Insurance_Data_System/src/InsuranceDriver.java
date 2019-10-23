8e396449ad581bcd66c3940ab0cf65f17c1d937e
joshchukwuezi <c18709101@mydit.ie>
2019-10-23 17:52:35 +0100

    Updated driver class

--- Insurance_Data_System/src/InsuranceDriver.java
+++ Insurance_Data_System/src/InsuranceDriver.java
@@ -1,9 +1,29 @@
+import java.util.ArrayList;	
+import java.util.Scanner;
 
-public class InsuranceDriver {
+public class InsuranceDriver { //class to drive the entire program
+	
+String plateNumber;
+String carYear; 
+boolean hadAccident;
+
+public InsuranceDriver() {
+ArrayList<Car> cars = new ArrayList<Car>();	//arrayList to store cars
+	
+}
+	
+ public void addCar(){
+	 Scanner scan = new Scanner (System.in);
+	 
+	System.out.println("Enter the plate number of the car");
+	plateNumber = scan.nextLine();
+	
+	System.out.println("Enter the year of the car");
+	carYear = scan.nextLine();
 	
 	
-	
-	
+	 
+ }
 	
 	
 	
