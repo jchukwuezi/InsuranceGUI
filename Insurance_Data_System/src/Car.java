78aa6fc95640bf3fcb65ab6d6a57d7cc7e032b9f
joshchukwuezi <c18709101@mydit.ie>
2019-10-24 15:46:27 +0100

    Updated GUI Class

--- Insurance_Data_System/src/Car.java
+++ Insurance_Data_System/src/Car.java
@@ -5,46 +5,45 @@ Student number: C18709101
 Course Code: DT354/2
 /////////////////////*/
 
-public class Car { //car class to create objects for the system
-String plateNumber;
-String carYear;
-boolean hadAccident;
+public class Car { // car class to create objects for the system
+	private String plateNumber;
+	private String carYear;
+	private boolean hadAccident;
 
-public Car(String pn, String cy, boolean ha) {
-	this.plateNumber=pn;
-	this.carYear=cy;
-	this.hadAccident=ha;
-}
+	public Car(String pn, String cy, boolean ha) {
+		this.plateNumber = pn;
+		this.carYear = cy;
+		this.hadAccident = ha;
+	}
 
 //getters and setters
-public String getPlateNumber() {
-	return plateNumber;
-}
+	public String getPlateNumber() {
+		return plateNumber;
+	}
 
-public void setPlateNumber(String plateNumber) {
-	this.plateNumber = plateNumber;
-}
+	public void setPlateNumber(String plateNumber) {
+		this.plateNumber = plateNumber;
+	}
 
-public String getCarYear() {
-	return carYear;
-}
+	public String getCarYear() {
+		return carYear;
+	}
 
-public void setCarYear(String carYear) {
-	this.carYear = carYear;
-}
+	public void setCarYear(String carYear) {
+		this.carYear = carYear;
+	}
 
-public boolean isHadAccident() {
-	return hadAccident;
-}
+	public boolean isHadAccident() {
+		return hadAccident;
+	}
 
-public void setHadAccident(boolean hadAccident) {
-	this.hadAccident = hadAccident;
-}
+	public void setHadAccident(boolean hadAccident) {
+		this.hadAccident = hadAccident;
+	}
 
-public String toString() {
-	return "\nPlate Number= " + this.plateNumber + "\nCar Year= " + this.carYear + "\nHas the Vehicle had an Accident? = " + this.hadAccident;
-}
-
-
+	public String toString() {
+		return "\nPlate Number= " + this.plateNumber + "\nCar Year= " + this.carYear
+				+ "\nHas the Vehicle had an Accident? = " + this.hadAccident;
+	}
 
 }
--- Insurance_Data_System/src/InsuranceGUI.java
+++ Insurance_Data_System/src/InsuranceGUI.java
@@ -1,4 +1,68 @@
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class InsuranceGUI {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SpinnerListModel;
+
+public class InsuranceGUI extends JFrame implements ActionListener {
+
+	JFrame insurance = new JFrame("Insurance Data System GUI");
+	Container content = insurance.getContentPane();
+
+	// add components
+	JButton enterCar = new JButton("Press to Enter a New Car");
+	
+	JTextField plateNumber = new JTextField();
+	JLabel pn = new JLabel ("Enter plate number");
+
+			// spinner with years
+	String years[] = { "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010", "2009" };
+	JSpinner carYear= new JSpinner(new SpinnerListModel(years));
+
+			// radio button for if car has had an accident
+			yesRadioButton=new JRadioButton();yesRadioButton.setText("Yes");
+
+			noRadioButton=new JRadioButton();noRadioButton.setText("No");
+	
+			BG = new ButtonGroup();
+			BG.add(yesRadioButton);
+			BG.add(noRadioButton);
+	
+			//text area to display
+			JTextArea display = new JTextArea(); //this will display the arraylist of cars with details
+	
+			//search for a car
+			JButton searchCar = new JButton("Seach for a car");
+			
+			JTextField carPlate = new JTextField(); 
+			JLabel cp = new JLabel ("Enter plate number");
+			
+			JTextArea carFound = new JTextArea();
+			
+			JOptionPane carNotFound = new JOptionPane("Car not found!",JOptionPane.ERROR_MESSAGE,JOptionPane.DEFAULT_OPTION);
+			
+			
+			//quit the application
+	         JButton quitApp = new JButton("Quit application");
+	         
+	         
+	         
+	
+
+	public static void main(String[] args) {
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
